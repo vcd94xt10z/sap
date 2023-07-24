f92c395a13ce08ba157d9d124dd58263cbcc0559
Vinicius Cesar Dias <vcd94xt10z@users.noreply.github.com>
2023-07-24 08:53:42 -0300

    Update Test.java

--- jco/Test.java
+++ jco/Test.java
@@ -2,44 +2,30 @@ package main;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.io.Writer;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.Properties;
 
-import com.sap.conn.jco.JCoAbapObject;
 import com.sap.conn.jco.JCoDestination;
 import com.sap.conn.jco.JCoDestinationManager;
-import com.sap.conn.jco.JCoField;
-import com.sap.conn.jco.JCoFieldIterator;
 import com.sap.conn.jco.JCoFunction;
-import com.sap.conn.jco.JCoMetaData;
-import com.sap.conn.jco.JCoParameterField;
-import com.sap.conn.jco.JCoParameterFieldIterator;
-import com.sap.conn.jco.JCoParameterList;
-import com.sap.conn.jco.JCoRecord;
-import com.sap.conn.jco.JCoRecordFieldIterator;
-import com.sap.conn.jco.JCoRecordMetaData;
 import com.sap.conn.jco.JCoRepository;
 import com.sap.conn.jco.JCoStructure;
 import com.sap.conn.jco.JCoTable;
 import com.sap.conn.jco.ext.DestinationDataProvider;
 
+/**
+ * @author Vinicius Cesar Dias
+ * https://github.com/vcd94xt10z
+ */
 public class Test {
 
 	public static void main(String[] args) {
 		try {
-			String DESTINATION_SAP1 = "SAPSYSTEM1";
+			String DESTINATION_SAP = "DEV";
 			
-			createDestinationDataFile(DESTINATION_SAP1);
+			createDestinationDataFile(DESTINATION_SAP);
 			
 			final String functionModule = "ZFM_TEST_RFC";
-	        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_SAP1);
+	        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_SAP);
 	        JCoFunction function = destination.getRepository().getFunction(functionModule);
 	        if (function == null) {
 	            throw new RuntimeException(functionModule+" não encontrada");
@@ -51,34 +37,34 @@ public class Test {
 			repository.clear();
 	        
 			// importação: parâmetros simples
-	        function.getImportParameterList().setValue("ID_PARAM1", "Teste");
+	        function.getImportParameterList().setValue("ID_PARAM1", "Teste ID_PARAM1");
 	        
 	        // importação: estruturas
 	        JCoStructure importStructure = function.getImportParameterList().getStructure("IS_SAIRPORT");
-	        importStructure.setValue("ID","ABC");
-	        importStructure.setValue("NAME","Aeroporto ABC");
-	        importStructure.setValue("TIME_ZONE","UTC+9");
+	        importStructure.setValue("ID","A01");
+	        importStructure.setValue("NAME","Aeroporto A01");
+	        importStructure.setValue("TIME_ZONE","UTC+1");
 	        
 	        // modificação: parâmetros simples
-	        function.getChangingParameterList().setValue("CD_PARAM1", 1);
+	        function.getChangingParameterList().setValue("CD_PARAM1", 2);
 	        
 	        // modificação: estruturas
 	        JCoStructure changingStructure = function.getChangingParameterList().getStructure("CS_SAIRPORT");
-	        changingStructure.setValue("ID","ABC");
-	        changingStructure.setValue("NAME","Aeroporto ABC");
-	        changingStructure.setValue("TIME_ZONE","UTC+1");
+	        changingStructure.setValue("ID","A02");
+	        changingStructure.setValue("NAME","Aeroporto A02");
+	        changingStructure.setValue("TIME_ZONE","UTC+3");
 	        
 	        // tabelas de entrada
 	        JCoTable importTable = function.getTableParameterList().getTable("IT_SAIRPORT");
 	        importTable.insertRow(1);
-	        importTable.setValue(1, "A01");
-	        importTable.setValue(2, "Aeroporto A01");
-	        importTable.setValue(3, "UTF-1");
+	        importTable.setValue(1, "A03");
+	        importTable.setValue(2, "Aeroporto A03");
+	        importTable.setValue(3, "UTF-4");
 	        
 	        importTable.insertRow(2);
-	        importTable.setValue(1, "A02");
-	        importTable.setValue(2, "Aeroporto A02");
-	        importTable.setValue(3, "UTF-2");
+	        importTable.setValue(1, "A04");
+	        importTable.setValue(2, "Aeroporto A04");
+	        importTable.setValue(3, "UTF-5");
 	        
 	        // executando chamada remota
 	        function.execute(destination);
