be36be22c40e990803157d25b745cf51d2f7247c
Vinicius Cesar Dias <vcd94xt10z@users.noreply.github.com>
2023-07-24 00:34:27 -0300

    Update index.js

--- node-rfc/index.js
+++ node-rfc/index.js
@@ -1,3 +1,5 @@
+// diretório nwrfcsdk deve existir na pasta do node
+// sapnwrfc.ini deve existir na pasta do node
 const nodeRfc = require('node-rfc');
 const pool = new nodeRfc.Pool({ connectionParameters: { dest: "S4H" }});
 
@@ -5,10 +7,31 @@ const pool = new nodeRfc.Pool({ connectionParameters: { dest: "S4H" }});
     try {
         const client = await pool.acquire();
 
-        const result = await client.call("BAPI_USER_GET_DETAIL", {
-            USERNAME: "DEVELOPER",
+        //const result = await client.call("BAPI_USER_GET_DETAIL", {
+        //    USERNAME: "DEVELOPER",
+        //});
+
+		const result = await client.call("ZFM_TEST_RFC", {
+            ID_PARAM1: "1",
+			IS_SAIRPORT: {
+				'ID': 'ABC'
+			},
+			CS_SAIRPORT: {
+				'ID': 'ABC'
+			},
+			CD_PARAM1: 1,
+			IT_SAIRPORT: [
+			{
+				'ID': 'AAA'
+			},
+			{
+				'ID': 'AAA'
+			}
+			
+			]
         });
 
+			
         console.log(result);
     
     } catch (err) {
